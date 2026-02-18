c682a837d206c3da7fef4a41cb0b28a60fa4031e
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-18 16:51:04 +0300

    Промежуточный коммит для дальнейшей работы над фичей copy

--- cli.py
+++ cli.py
@@ -16,10 +16,20 @@ def main():
     menu_parser = subparsers.add_parser('menu', help='Запустить интерактивный интерфейс')
 
     # Здесь пока пусто — команды будем добавлять в feature-ветках!
+        # --- КОМАНДЫ ДЛЯ ТЗ (Утилиты) ---
 
+    # Команда 1: Копирователь файла
+    copy_p = subparsers.add_parser('copy', help='Скопировать файл')
+    copy_p.add_argument('file_name', type=str, help='Имя файла, который нужно найти и скопировать')
     args = parser.parse_args()
 
-    if args.command is None:
+        # ЛОГИКА ВЫБОРА
+    if args.command == 'menu':
+        start_menu()  # Запускаем интерактивное меню!
+    elif args.command == 'copy':
+        copy_file(args.file_name)
+    
+    else args.command is None:
         parser.print_help()
 
 if __name__ == "__main__":

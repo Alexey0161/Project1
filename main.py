336c6ce2ef9c530637f4bafcbc3a1b7a28e44a8e
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-18 17:26:22 +0300

    Внес в файл main.py в ветке dev  код по запуску из интерактивного меню фичи - копировальщика файлов

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
+import sys
 import logging
-
+# импортируем функции фичей из соответствующих файлов
+from src.filesystem.cli_copy_files import copy_file
 
 def main():
     print("--- Мой Супер Проект (Project1) ---")
@@ -9,6 +12,15 @@ def main():
     print("3. Запустить поисковик файлов")
     print("4. Запустить установщик даты в имя файла")
 
+    choice = input("Введите номер: ")
+    if choice == "1":
+        print('----Запускает копировальщик файлов ----')
+        try:
+            filename = input('Введите имя файла, который надо скопировать: ')
+            copy_file(filename)
+        except Exception as e:
+                
+                print(e)
 
 # Магическая проверка: запущен ли файл напрямую
 if __name__ == "__main__":

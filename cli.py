e210af57da11ef01df2e980734b2d52706fc6b69
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-18 17:15:34 +0300

    Собрал в cli.py код запуска фичи copy из ветки feature-copy, проверил ---> работает

--- cli.py
+++ cli.py
@@ -1,19 +1,39 @@
 import argparse
-import logging
 from src.filesystem.cli_copy_files import copy_file
 
-# Настраиваем логирование (как мы с вами учили!)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Импортируем модуль логирования
+import logging
+
+# Настройка "голоса"  программы в логировании
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%H:%M:%S' # Чтобы не загромождать дату, оставим только время
+)
 
 def main():
-    parser = argparse.ArgumentParser(description="My Cool Tool")
-    subparsers = parser.add_subparsers(dest="command", help="Доступные команды")
+    parser = argparse.ArgumentParser(description="Project1: Универсальный инструмент")
+    subparsers = parser.add_subparsers(dest="command", help="Команды")
 
-    # Здесь пока пусто — команды будем добавлять в feature-ветках!
+    # --- КОМАНДА ДЛЯ ДУШИ (Ваше меню) делаем add_parser для меню---
+    menu_parser = subparsers.add_parser('menu', help='Запустить интерактивный интерфейс')
 
+       # --- КОМАНДЫ ДЛЯ ТЗ (Утилиты) ---
+
+    # Команда 1: Копирователь файла
+    copy_p = subparsers.add_parser('copy', help='Скопировать файл')
+    copy_p.add_argument('file_name', type=str, help='Имя файла, который нужно найти и скопировать')
+
+    
     args = parser.parse_args()
+    
+        # ЛОГИКА ВЫБОРА
+    if args.command == 'menu':
+        start_menu()  # Запускаем интерактивное меню!
+    elif args.command == 'copy':
+        copy_file(args.file_name)
 
-    if args.command is None:
+    elif args.command is None:
         parser.print_help()
 
 if __name__ == "__main__":

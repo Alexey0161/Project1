594ceb6d170429cb82a7c2d5bc3144ca1224bef1
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-18 16:40:56 +0300

    Доработал основу cli.py для добавления фичей

--- cli.py
+++ cli.py
@@ -1,12 +1,19 @@
 import argparse
 import logging
+from src.filesystem.cli_copy_files import copy_file
 
-# Настраиваем логирование (как мы с вами учили!)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
+# Настройка "голоса"  программы в логировании
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%H:%M:%S' # Чтобы не загромождать дату, оставим только время
+)
 def main():
-    parser = argparse.ArgumentParser(description="My Cool Tool")
-    subparsers = parser.add_subparsers(dest="command", help="Доступные команды")
+    parser = argparse.ArgumentParser(description="Project1: Универсальный инструмент")
+    subparsers = parser.add_subparsers(dest="command", help="Команды")
+
+    # --- КОМАНДА ДЛЯ ДУШИ (Ваше меню) делаем add_parser для меню---
+    menu_parser = subparsers.add_parser('menu', help='Запустить интерактивный интерфейс')
 
     # Здесь пока пусто — команды будем добавлять в feature-ветках!
 

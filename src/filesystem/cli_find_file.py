56e9bc52c88d77f81417be5dcdc910983d7953fa
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-19 15:51:15 +0300

    Затянул в feature-find  файл cli_find_file.py

---
+++ src/filesystem/cli_find_file.py
@@ -0,0 +1,39 @@
+r""" --- чтобы в комментарии можно было писать любые пути через слэш "\", не экранируя
+Документация:
+запуск файла из командной строки из папки src:
+ python cli_find_file.py find <путь до целевой папки> <верхний предел размера файла в Кбайтах>
+ Пример:
+    python cli_find_file.py find /storage/emulated/0/python/tests 100
+"""
+
+import os
+import sys
+import logging
+
+def find_file(target_dir, size):
+    target_dir = os.path.normpath(target_dir)
+    limit_size = int(size) * 1024 #преобразуем кбайты в байты
+    t = [] # создаем список для файлов, которые прошли фильтр
+    for r, d, f in os.walk(target_dir):
+        for i in f: # f - walk выдает файлы в виде списка
+
+            path_i = os.path.join(r,i) # соединяем черз инструмент path
+                                       # путь и имя файла
+            if os.path.isfile(path_i):
+
+                full_size = os.path.getsize(path_i)
+
+                if full_size < limit_size:
+                    t.append(i)
+                    logging.info(f'Найден файл: {i} {full_size / 1024: .2f}')
+    return t
+
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv)  >= 2:
+        find_file(sys.argv[1], sys.argv[2])
+    else:
+        logging.warning("Используйте: python cli.py  <имя_папки> <размер>")
+

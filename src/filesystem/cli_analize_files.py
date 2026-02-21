19c0d7614fc6a46711d151c3ac9e738ee33464c3
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-02-22 00:16:09 +0300

    22.02.26 00:14 Собрал код фичи analize с подсчетом за один проход и размера ффайлов и размера вложенных папок, оставил временные принты, чтобы можно было откатиться, если Джемини не примет код и далее его править

--- src/filesystem/cli_analize_files.py
+++ src/filesystem/cli_analize_files.py
@@ -4,19 +4,44 @@ import sys
 import argparse
 
 def get_dir_size(path):
+    cnt = 0
     total = 0
-    total_local = 0
+    total_local = {}
+    
     dict_for_dir = {}
     for root, dirs, files in os.walk(path):
         name = os.path.basename(root)
+        # print(name, dirs, files, 14)
+        if cnt == 0 and len(dirs) > 0:
+            for i in dirs:
+                dict_for_dir[i] = [0]
+            # print(dict_for_dir, 18)
+        
         for f in files:
             fp = os.path.join(root, f)
-            total += os.path.getsize(fp)
-            total_local += os.path.getsize(fp)
-        dict_for_dir[name] = total_local
-        total_local = 0
+            size_file = os.path.getsize(fp)
+            total += size_file
+            # print(total, 23)
+            # for j in dict_for_dir.keys():
+            #     if j in root:
+            #         print(total,25)
+            #         dict_for_dir[j].append(total)
+            if cnt > 0:
+                for j in dict_for_dir.keys():
+                    # print(j,25)
+                    if j in root:
+                        # print(total,31)
+                        dict_for_dir[j].append(size_file)
+        cnt += 1
+        # if cnt == 3:
+            
+            
+    # print(dict_for_dir,38)
+    for k in dict_for_dir.keys():
+        dict_for_dir[k] = sum(dict_for_dir[k])
+    # print(dict_for_dir,40)
 
-    return total, dict_for_dir
+    return total, dict_for_dir#, total_local
 
 def format_size(size_bytes):
     # Определяем пороги для перехода на следующую единицу
@@ -41,18 +66,26 @@ def analize_files(root_path):
         
             raise FileNotFoundError(f"Ошибка: Путь {root_path} не существует.")
     else:
-        
-        print(f'full size: {format_size(get_dir_size(root_path)[0])}')
+        result = get_dir_size(root_path)#[0], get_dir_size(root_path)[1]
+        full_size = result[0]
+        dict_for_dir = result[1]
+        # print(full_size, 54)
+        # print(dict_for_dir, 55)
+        # print(total_local, 48)
+        print(f'full size: {format_size(full_size)}')
         for item in os.listdir(root_path):
+            # print(item,75)
             full_path = os.path.join(root_path, item)
             if os.path.isfile(full_path):
                 size = os.path.getsize(full_path)
                 print(f'-file: {item:<10} {format_size(size):>10}')
             else:
-                if item != name:
-                    print(f'-folder: {item:<10}  {format_size(get_dir_size(root_path)[1][item]):>10}') # редактирую сейчас
-        #         size = get_dir_size(full_path) # Вот она, магия анализа вложенности!
-        #         print(f'-folder: {item:<10}  {format_size(size):>10}')
+
+            #         print(item, 56)
+            #         pass
+            #         print(f'-folder: {item:<10}  {format_size(dict_folder[item]):>10}') # редактирую сейчас
+                size = dict_for_dir[item] # Вот она, магия анализа вложенности!
+                print(f'-folder: {item:<10}  {format_size(size):>10}')
     
 
 if __name__ == '__main__':

9a153e121190f45302da840296bda23aea0b574c
Alexey Alexey <alexey28.01.1989@gmail.com>
2026-03-09 20:26:08 +0300

    09.03.26 20:24 Добавил config.py в src провелил работу константы - 1024 и логирования. Все Ок.

--- src/filesystem/cli_find_file.py
+++ src/filesystem/cli_find_file.py
@@ -1,14 +1,10 @@
-
-
 import os
 import sys
 import logging
-# import config
-from config import BYTES_PER_KB 
+from src.config import BYTES_PER_KB, setup_logging 
 
 def find_file(target_dir, size):
     target_dir = os.path.normpath(target_dir)
-    # print(target_dir, size, 9)
     limit_size = None
     try:
         limit_size = float(size) * BYTES_PER_KB  #преобразуем кбайты в байты
@@ -26,27 +22,23 @@ def find_file(target_dir, size):
                 if os.path.isfile(path_i):
 
                     full_size = os.path.getsize(path_i)
-                    # print(full_size, 27)
 
                     if  limit_size is not None:
                         if full_size < limit_size:
                             found_files.append(i)
                             logging.info(f'Найден файл: {i} {full_size / BYTES_PER_KB: .2f}')
-                            print(f'Найден файл: {i} {full_size / 1024: .2f}')
+                            
                     else:
                         return
-        # print(found_files, 35)                         
     else: 
         raise FileNotFoundError(f"Ошибка: Путь {target_dir} не существует.")
         
     return found_files
 
-find_file('C:\\Users\\ivano\\Desktop\\Project1\\Total1', 1)
-
-# if __name__ == '__main__':
-
-#     if len(sys.argv)  >= 2:
-#         find_file(sys.argv[1], sys.argv[2])
-#     else:
-#         logging.warning("Используйте: python cli.py  <имя_папки> <размер>")
+if __name__ == '__main__':
+    setup_logging()
+    if len(sys.argv)  >= 2:
+        find_file(sys.argv[1], sys.argv[2])
+    else:
+        logging.warning("Используйте: python cli.py  <имя_папки> <размер>")
 
